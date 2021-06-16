0a6d237d59e8a1e920eb4266aff9c579804eab3a
Hanni Suka <hannisuka99@hotmail.com>
2021-06-16 12:12:19 +0700

    Menambahkan dan Meletekkan Komponen

--- src/GuiByText.java
+++ src/GuiByText.java
@@ -1,14 +1,29 @@
 import javax.swing.*;
 
 public class GuiByText extends JFrame {
+
+    JLabel label1 = new JLabel("Coba Input1");
+    JTextField textField1 = new JTextField();
+
     GuiByText(){
         setSize(640,480);
         setLocationRelativeTo(null);
         setTitle("Gui By Text");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
+    void meletakkanKomponen(){
+        getContentPane().setLayout(null);
+
+        getContentPane().add(label1);
+        getContentPane().add(textField1);
+
+        label1.setBounds(16,32,100,25);
+        textField1.setBounds(125,32,100,25);
+    }
     public static void main(String[] args){
         GuiByText guiByText = new GuiByText();
+        guiByText.meletakkanKomponen();
         guiByText.setVisible(true);
     }
 }
